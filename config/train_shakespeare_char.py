c80453763edf0e7c345f948d0b81affa452a80b1
Alexis Contour <alex@glumol.com>
2023-03-08 21:59:24 +0100

    train_shakespeare

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -4,14 +4,14 @@
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
-log_interval = 10 # don't print too too often
+log_interval = 1 # don't print too too often
 
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt'
+wandb_log = True # override via command line if you like
+wandb_project = 'gpt-mods'
+wandb_run_name = 'shakespeare-baseline'
 
 dataset = 'shakespeare_char'
 batch_size = 64
@@ -23,13 +23,13 @@ n_head = 6
 n_embd = 384
 dropout = 0.2
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+learning_rate = 5e-3 # with baby networks can afford to go a bit higher
+max_iters = 1000
+lr_decay_iters = 1000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
-warmup_iters = 100 # not super necessary potentially
+warmup_iters = 50 # not super necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
